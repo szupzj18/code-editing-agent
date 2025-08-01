54d9ddc118bcf9b10033f99a882c5d2ab94857ca
panzijian.chris <panzijian.chris@bytedance.com>
2025-08-01 14:02:12 +0800

    init

---
+++ main.go
@@ -0,0 +1,81 @@
+package code_editing_agent
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/anthropics/anthropic-sdk-go"
+	"os"
+)
+
+func main() {
+	client := anthropic.NewClient(os.Getenv("ANTHROIC_ACCESS_TOKEN"))
+	scanner := bufio.NewScanner(os.Stdin)
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMessage)
+	err := agent.Run(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %s\n\n", err)
+	}
+
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client         *anthropic.Client
+	getUserMessage func() (string, bool)
+}
+
+type MessageParam struct {
+}
+
+func (a Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MessageParam{}
+
+	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
+	for {
+		fmt.Print("\u001b[94mYou\u001b[0m: ")
+		userInput, ok := a.getUserMessage()
+		if !ok {
+			break
+		}
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		conversation = append(conversation, userMessage)
+
+		message, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, message.ToParam())
+
+		for _, content := range message.Content {
+			switch content.Type {
+			case "text":
+				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (a Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+	message, error := a.client.Messages.New(ctx, anthropic.MessageNewParams{
+		Model:     anthropic.ModelClaude3_7SonnetLatest,
+		MaxTokens: int64(1024),
+		Messages:  conversation,
+	})
+	return message, error
+}
